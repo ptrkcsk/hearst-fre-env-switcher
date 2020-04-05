2f7a80a29e0f41821f6ede64b5ff337dc0e57725
Patrik Csak <patrik.csak@hearst.com>
2020-04-05 14:16:30 -0400

    Add dark mode support

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { CssBaseline, useMediaQuery } from '@material-ui/core'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import React from 'react'
 import EnvList from './EnvList'
 
 function App () {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  )
+
   return (
-    <EnvList/>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <EnvList/>
+    </ThemeProvider>
   )
 }
 
