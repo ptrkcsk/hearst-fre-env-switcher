2b51714914c6abf3b27c3aac5ae56b950567b327
Patrik Csak <patrik.csak@hearst.com>
2020-04-19 10:07:14 -0400

    Remove unused code

--- src/lib/get-tld.js
+++
@@ -1,9 +0,0 @@
-import { parse_host as tldExtract } from 'tld-extract'
-
-/**
- * @param {string} hostname
- * @return {string}
- */
-export default function getTld (hostname) {
-  return tldExtract(hostname).tld
-}
